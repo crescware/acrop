69b1d0c051933914d42477da2c7cb6e21010d2dc
OKUNOKENTARO <okunokentaro@me.com>
2025-05-06 00:02:09 +0900

    add tests

---
+++ src/testing/main-unmatched-patterns-2.test.ts
@@ -0,0 +1,42 @@
+import type { InferOutput } from "valibot";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+
+import type { config$ } from "../import-config/config";
+import { main } from "../main";
+import { setupMainTest } from "./test-utils";
+
+vi.mock("../import-config", () => ({ importConfig: vi.fn() }));
+
+const testConfig: InferOutput<typeof config$>["default"] = {
+	root: ".",
+	scopes: [
+		{
+			scope: "./a/**/*",
+			rules: [{ allowed: ["./a/**/*", "./unused/**/*"] }],
+		},
+	],
+};
+
+setupMainTest({
+	configFileName: "acrop.config.ts",
+	config: testConfig,
+	files: [{ path: "a/a.ts", content: "" }],
+});
+
+describe.each([
+	{ severity: "warn", expectSuccess: true },
+	{ severity: "error", expectSuccess: false },
+] as const)(
+	"main() – unmatched-patterns: $severity",
+	({ severity, expectSuccess }) => {
+		let success: boolean;
+		beforeEach(async () => {
+			process.argv.push("--unmatched-patterns", severity);
+			success = await main();
+		});
+
+		test(`should return ${expectSuccess}`, () => {
+			expect(success).toBe(expectSuccess);
+		});
+	},
+);
